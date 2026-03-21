12ce234c4c67a2cbdbfda66595076adc87ec212e
rgilks <robert.gilks@cambridge.org>
2026-03-21 09:48:49 +0000

    Use full-height app screen layouts

--- src/client/main.ts
+++ src/client/main.ts
@@ -118,7 +118,6 @@ import { installGlobalErrorHandlers, track } from './telemetry';
 import { Tutorial } from './tutorial';
 import type { UIEvent } from './ui/events';
 import { UIManager } from './ui/ui';
-import { installViewportSizing } from './viewport';
 
 interface ClientContext {
   state: ClientState;
@@ -1531,8 +1530,6 @@ class GameClient {
 
 // --- Bootstrap ---
 installGlobalErrorHandlers();
-
-installViewportSizing();
 
 const __game = new GameClient();
 (window as any).__game = __game;
